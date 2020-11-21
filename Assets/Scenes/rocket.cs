5ea522d74e404cd2ab4ceb00750addf12fb429ba
Uko1ove <a.ukolov@gmail.com>
2020-11-21 15:59:02 +0300

    add script, rocket fly and fly sound

---
+++ Assets/Scenes/rocket.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    AudioSource rocketThrust;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rocketThrust = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+
+    private void processInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (!rocketThrust.isPlaying)
+            {
+                rocketThrust.Play();
+            }
+        }
+        else
+        {
+            rocketThrust.Stop();
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
